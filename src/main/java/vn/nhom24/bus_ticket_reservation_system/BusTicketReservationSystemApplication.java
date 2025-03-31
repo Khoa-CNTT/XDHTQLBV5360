eac70d009373adcf0b35f68f7d04cee6706396be
tuapluctt <hoangvantunkv@gmail.com>
2025-03-31 16:56:08 +0700

    Initial commit for new project

---
+++ src/main/java/vn/nhom24/bus_ticket_reservation_system/BusTicketReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package vn.nhom24.bus_ticket_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketReservationSystemApplication.class, args);
+	}
+
+}
